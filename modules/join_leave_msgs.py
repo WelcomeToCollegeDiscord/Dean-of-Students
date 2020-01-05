c6fff88670d82b694c4fe3d77d6ba625377397b7
SaitoH <LFGSaito@users.noreply.github.com>
2020-01-04 21:39:38 -0500

    Update join_leave_msgs.py

--- modules/join_leave_msgs.py
+++ modules/join_leave_msgs.py
@@ -5,16 +5,16 @@ import discord
 
 @client.member_join
 async def join_notification(member: discord.Member):
-    if "discord.gg" in member.name and member.guild.id == 458765854624972811:
+    if "discord.gg" in member.name and member.guild.id == 662417141080916001:
         try:
             await member.ban()
         except:
-            await client.get_channel(643340226894233610).send(
+            await client.get_channel(663209067900174336).send(
                 "Attempted to autoban user below for \"discord.gg\" in username but failed!")
         else:
-            await client.get_channel(643340226894233610).send(
+            await client.get_channel(663209067900174336).send(
                 "Successfully autobanned below user for \"discord.gg\" in username.")
-    if member.guild.id == 458765854624972811:
+    if member.guild.id == 662417141080916001:
         with open("logs/members.log", "a") as lf:
             lf.write(f"{member.joined_at.__str__()}+{member.guild.member_count}\n")
         embed = discord.Embed(title="Member has joined the server", description=discord.Embed.Empty, colour=0x15a216)
@@ -27,12 +27,12 @@ async def join_notification(member: discord.Member):
         embed = embed.add_field(name="Account Created At",
                                 value=f"{created_ts.__str__()}\n{str(datetime.utcnow() - created_ts)}")
         embed = embed.set_footer(text=member.joined_at)
-        await client.get_channel(643340226894233610).send(embed=embed)
+        await client.get_channel(663209067900174336).send(embed=embed)
 
 
 @client.member_remove
 async def leave_notification(member: discord.Member):
-    if member.guild.id == 458765854624972811:
+    if member.guild.id == 662417141080916001:
         with open("logs/members.log", "a") as lf:
             lf.write(f"{datetime.utcnow().__str__()}-{member.guild.member_count}\n")
         now = datetime.utcnow()
@@ -46,4 +46,4 @@ async def leave_notification(member: discord.Member):
         embed = embed.add_field(name="Member since",
                                 value=f"{member.joined_at.__str__()} UTC ({now - member.joined_at})")
         embed = embed.set_footer(text=str(now))
-        await client.get_channel(643340226894233610).send(embed=embed)
+        await client.get_channel(663209067900174336).send(embed=embed)
