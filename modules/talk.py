0cb9c7d6481bf06513c0ae6ad7bea9d5a17fceb1
Saito <LFGSaito@users.noreply.github.com>
2020-01-28 13:30:59 -0500

    YES YES YES

--- modules/talk.py
+++ modules/talk.py
@@ -4,8 +4,7 @@ import discord
 
 @client.message()
 async def talk(command: str, message: discord.Message):
-    ch_id = command[2:]
-    text = command[3:]
+    ch_id, text = command.split("", 1)
 
     channel = client.get_channel(ch_id)
     if channel is not None and message.author.id("195582200270290944"):
