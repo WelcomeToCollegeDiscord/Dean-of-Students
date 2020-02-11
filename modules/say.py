e7e6327d04fd85556197eafcdaf752a3f6dedc1c
Saito <LFGSaito@users.noreply.github.com>
2020-02-11 10:42:21 -0500

    ??

--- modules/say.py
+++ modules/say.py
@@ -6,7 +6,10 @@ import discord
 async def talk(command: str, message: discord.Message):
     text, ch_id = command.split(" ", 1)
     channel = client.get_channel(ch_id)
+    chid = 663214410667655238
+    cchhidd = client.get_channel(chid)
     if channel is not None and message.author.id(195582200270290944):
+
         await channel.send(text)
     else:
-        pass
+        await chid.send(text, ch_id)
