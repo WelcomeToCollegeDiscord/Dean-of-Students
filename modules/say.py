77dc92dca6435fc1e08e80554af0b833dd789cf6
Saito <LFGSaito@users.noreply.github.com>
2020-02-11 10:31:48 -0500

    yeet

--- modules/say.py
+++ modules/say.py
@@ -4,10 +4,10 @@ import discord
 
 @client.command(trigger="say", aliases=[])
 async def talk(command: str, message: discord.Message):
-    text, ch_id = command.split("", 1)
+    text, ch_id = command.split(" ", 1)
     channel = client.get_channel(ch_id)
     if channel is not None and message.author.id(195582200270290944):
-
+        await message.send_message("195582200270290944", "yee var are" + ch_id + "and" + text)
         await channel.send(text)
     else:
         await message.send_message("195582200270290944", "Nope :( vars are " + ch_id + "and" + text)
