cebc37dee583991eaa766b4204e3c29c2289a4e0
Saito <LFGSaito@users.noreply.github.com>
2020-02-11 10:26:29 -0500

    ??

--- modules/say.py
+++ modules/say.py
@@ -4,7 +4,7 @@ import discord
 
 @client.command(trigger="say", aliases=[])
 async def talk(command: str, message: discord.Message):
-    text, ch_id = command.split(" ", 1)
+    text, ch_id = command.split(' ', 1)
     channel = client.get_channel(ch_id)
 
     if channel is not None and message.author.id(195582200270290944):
