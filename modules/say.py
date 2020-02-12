9d5a1bd6b035f328735f912c447c3b74ff993900
Saito <LFGSaito@users.noreply.github.com>
2020-02-12 13:02:53 -0500

    ??

--- modules/say.py
+++ modules/say.py
@@ -6,14 +6,14 @@ cmd_name = "talk"
 
 @client.command(trigger=cmd_name,
                 aliases=["say"])  # aliases is a list of strs of other triggers for the command
-async def command(command: str, message: discord.Message):
+async def command(command: str, msg: discord.Message, user: discord.User):
     ch_id, text = command.split(" ", 1)
 
     channel = client.get_channel(ch_id)
-    if channel is not None and message.author.id(195582200270290944):
+    if channel is not None and msg.author.id(195582200270290944):
         await channel.send(text)
     else:
-        user = await client.get_user_info("195582200270290944")
-        await client.send_message(user, "Vars are", ch_id, text)
+        message = text, ch_id
+        await client.send_message(user, message)
 
         raise TypeError("Invalid or non-existant channel ID (Not a text channel?)")
