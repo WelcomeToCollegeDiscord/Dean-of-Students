d7eecd4c1a7c4ca86a38bee59c3ec1992230d0ed
Saito <LFGSaito@users.noreply.github.com>
2020-02-12 21:36:45 -0500

    tewst

--- modules/say.py
+++ modules/say.py
@@ -7,7 +7,7 @@ cmd_name = "talk"
 @client.command(trigger=cmd_name,
                 aliases=["say"])  # aliases is a list of strs of other triggers for the command
 async def command(command: str, msg: discord.Message):
-    gui, ch_id, text = command.split(" ", 3)
+    gui, ch_id, text = command.split(" ", 4)
     channel = client.get_channel(ch_id)
     guild = client.get_guild(gui)
     if channel is not None:
