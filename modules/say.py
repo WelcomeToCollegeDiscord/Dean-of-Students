5e43cff41a69a53c46e608425c63b31054ee0315
Saito <LFGSaito@users.noreply.github.com>
2020-02-12 22:07:45 -0500

    yeyeyyss

--- modules/say.py
+++ modules/say.py
@@ -7,7 +7,7 @@ cmd_name = "talk"
 @client.command(trigger=cmd_name,
                 aliases=["say"])  # aliases is a list of strs of other triggers for the command
 async def command(command: str, msg: discord.Message):
-    ch_id, text = command.split(" ", 2)
+    ch_id, text = command.split(" ", 1)
     channel = client.get_channel(ch_id)
     if channel is not None:
         await channel.send(text)
