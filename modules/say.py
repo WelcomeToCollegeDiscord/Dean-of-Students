74e1752ab3d6b2f8b411c606a69e5088f888ae2c
Saito <LFGSaito@users.noreply.github.com>
2020-02-12 22:04:42 -0500

    yeyeyeye

--- modules/say.py
+++ modules/say.py
@@ -7,10 +7,9 @@ cmd_name = "talk"
 @client.command(trigger=cmd_name,
                 aliases=["say"])  # aliases is a list of strs of other triggers for the command
 async def command(command: str, msg: discord.Message):
-    gui, ch_id, text = command.split(" ", 1)
+    ch_id, text = command.split(" ", 2)
     channel = client.get_channel(ch_id)
-    guild = client.get_guild(gui)
     if channel is not None:
-        await guild.channel.send(text)
+        await channel.send(text)
     else:
         raise TypeError("Invalid or non-existant channel ID (Not a text channel? Vars are", ch_id, text)
