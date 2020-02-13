f6d7266a250462c3183dc2e1cd3f9b8c1dc5a1f8
Saito <LFGSaito@users.noreply.github.com>
2020-02-12 22:10:56 -0500

    yes

--- modules/say.py
+++ modules/say.py
@@ -9,7 +9,10 @@ cmd_name = "talk"
 async def command(command: str, msg: discord.Message):
     ch_id, text = command.split(" ", 1)
     channel = client.get_channel(ch_id)
+    oof = client.get_channel(663214410667655238)
+
     if channel is not None:
+        await oof.send("Vars are", ch_id. text)
         await channel.send(text)
     else:
         raise TypeError("Invalid or non-existant channel ID (Not a text channel? Vars are", ch_id, text)
