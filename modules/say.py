0cf17ac82e357d34ef0ccf9adc093aec7d73e40b
Saito <LFGSaito@users.noreply.github.com>
2020-02-13 09:23:47 -0500

    ??

--- modules/say.py
+++ modules/say.py
@@ -15,7 +15,7 @@ async def command(command: str, msg: discord.Message):
 
     channel = client.get_channel(ch_id)
 
-    if channel is not None:  # and message.author.id(195582200270290944):
+    if channel is not None and msg.author.id(195582200270290944):
         await channel.send(text)
     else:
         raise TypeError("Invalid or non-existant channel ID (Not a text channel? Vars are", ch_id, text)
