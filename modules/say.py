b6c517afaaa8bf694c6112334a95858c6964e8f8
Saito <LFGSaito@users.noreply.github.com>
2020-02-13 22:49:57 -0500

    GET OUT MY SMAP

--- modules/say.py
+++ modules/say.py
@@ -6,7 +6,7 @@ cmd_name = "talk"
 
 @client.command(trigger=cmd_name,
                 aliases=["say"])  # aliases is a list of strs of other triggers for the command
-async def command(command: str, msg: discord.Message):
+async def command(command: str, message: discord.Message):
     a1 = command.split(" ", 2)
     ch_idstr = a1[1]
     text = a1[2]
@@ -15,7 +15,7 @@ async def command(command: str, msg: discord.Message):
 
     channel = client.get_channel(daddyid)
 
-    if channel is not None and msg.author.id(195582200270290944):
+    if channel is not None and message.author.id(195582200270290944):
         await channel.send(text)
     else:
         raise TypeError("Invalid or non-existant channel ID (Not a text channel? Vars are", daddyid, text)
